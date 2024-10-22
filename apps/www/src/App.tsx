bf3f1e1b1bf8cc4125a55ef9cd024a16f80c290d
ofcljaved <javedofficialme@gmail.com>
2024-10-22 22:09:32 +0530

    fix: rose-pine theme

--- apps/www/src/App.tsx
+++ apps/www/src/App.tsx
@@ -1,6 +1,21 @@
 function App() {
     return (
-        <h1>hello</h1>
+        <>
+            <p className="space-x-4 text-center">
+                <span className="text-foreground">hell</span>
+                <span className="text-surface">hell</span>
+                <span className="text-overlay">hell</span>
+                <span className="text-muted">hell</span>
+                <span className="text-subtle">hell</span>
+                <span className="text-cursor">hell</span>
+                <span className="text-error">hell</span>
+                <span className="text-warning">hell</span>
+                <span className="text-success">hell</span>
+                <span className="text-selection-background">hell</span>
+                <span className="text-cursorline-background">hell</span>
+            </p>
+            <h1 className="text-red-300">hello</h1>
+        </>
     )
 }
 export default App;
