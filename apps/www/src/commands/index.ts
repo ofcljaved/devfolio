efe44b7a4a2a40894a531959a29b40e0088fc744
ofcljaved <javedofficialme@gmail.com>
2024-12-11 21:28:46 +0530

    feat: check for commands in pre defined command

---
+++ apps/www/src/commands/index.ts
@@ -0,0 +1,29 @@
+export const COMMANDS = ["help", "echo", "whoami"] as const;
+export type COMMAND_TYPE = (typeof COMMANDS)[number];
+
+type COMMANDS_HELP_TYPE = Record<
+  COMMAND_TYPE,
+  {
+    description: string,
+    example: string,
+    shortDesc: string,
+  }
+>;
+
+export const COMMANDS_HELP: COMMANDS_HELP_TYPE = {
+  help: {
+    description: `help commands show you the set of available commands in this terminal.\r\n using -h or --help with other command will display thier description and example usage`,
+    example: `help : shows you about, echo, whoami \r\n echo --help : shows how echo description`,
+    shortDesc: `shows you available set of commands`,
+  },
+  echo: {
+    description: `help commands show you the set of available commands in this terminal.\r\n using -h or --help with other command will display thier description and example usage`,
+    example: `help : shows you about, echo, whoami \r\n echo --help : shows how echo description`,
+    shortDesc: `shows you available set of commands`,
+  },
+  whoami: {
+    description: `help commands show you the set of available commands in this terminal.\r\n using -h or --help with other command will display thier description and example usage`,
+    example: `help : shows you about, echo, whoami \r\n echo --help : shows how echo description`,
+    shortDesc: `shows you available set of commands`,
+  },
+};
--- apps/www/src/components/FakeInput.tsx
+++ apps/www/src/components/FakeInput.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
+import { COMMANDS } from "~/commands";
 import { cn } from "~/lib/utils";
 
 export const FakeInput = ({ input }: { input: string }) => {
@@ -23,15 +24,17 @@ export const FakeInput = ({ input }: { input: string }) => {
         <div className="font-term">
             {!isEmpty && (
                 userCmd.map((cmd, idx) => {
+                    const isCmd = idx === 0 && (COMMANDS as readonly string[]).includes(cmd);
                     return (
-                        <span key={idx} className="text-error">
+                        <span key={idx} className={cn(isCmd ? "text-success" : "text-error")}>
                             {idx != 0 && " "}
                             {cmd}
+                            {isCmd}
                         </span>
                     )
                 })
             )}
-            <span className={cn(!isTyping && "animate-blink", "inline-block w-[1ch] h-[1em] align-text-bottom bg-foreground")}/>
+            <span className={cn(!isTyping && "animate-blink", "inline-block w-[1ch] h-[1em] align-text-bottom bg-foreground")} />
         </div>
     )
 }
