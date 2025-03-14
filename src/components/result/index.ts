444b8a69dba6a72543f467c7224526c607fb9c98
ofcljaved <javedofficialme@gmail.com>
2025-03-14 19:11:09 +0530

    chore: test3

--- src/components/result/index.ts
+++ src/components/result/index.ts
@@ -2,16 +2,16 @@ import { COMMAND_TYPE } from "@/commands";
 import { Echo } from "./Echo";
 import { Help } from "./Help";
 import { Whoami } from "./Whoami";
-import { Welcome } from "./Welcome";
+//import { Welcome } from "./Welcome";
 
 const result: Record<COMMAND_TYPE, React.ComponentType<{ args: string }>> = {
   help: Help,
   echo: Echo,
   whoami: Whoami,
-  welcome: Welcome,
-  projects: Welcome,
-  blog: Welcome,
-  education: Welcome,
-  about: Welcome,
+  welcome: Whoami,
+  projects: Whoami,
+  blog: Whoami,
+  education: Whoami,
+  about: Whoami,
 };
 export default result;
