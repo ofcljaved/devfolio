1b67e1794d216468c4b38aa1a617644909455dfa
ofcljaved <javedofficialme@gmail.com>
2025-03-25 18:30:25 +0530

    feat: improve seo and mobile support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import InputPrompt from "@/components/input/inputPrompt";
 import CommandExecuted from "@/components/output/commandExecuted";
+import { useEffect, useState } from "react";
+import { Pfp } from "@/components/pfp";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      const mobileQuery = window.matchMedia("(max-width: 768px)");
+      const userAgent = navigator.userAgent.toLowerCase();
+      const mobileDevices = /android|iphone|ipad|blackberry|windows phone/;
+      setIsMobile(mobileQuery.matches || mobileDevices.test(userAgent));
+    }
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, [])
+
+  if (isMobile) {
+    return (
+      <main className="p-4 font-term h-full overflow-y-auto flex flex-col gap-4 items-center">
+        <Pfp className="text-[8px] leading-[8px]" />
+        <h1 className="text-warning text-2xl">Desktop Only</h1>
+        <p className="text-success">Sorry, this terminal portfolio is not supported on mobile devices.</p>
+        <p>Please use a desktop or laptop to experience it as intended.</p>
+      </main>
+    );
+  }
+
   return (
     <main className="p-4 font-term h-full overflow-y-auto">
       <CommandExecuted />
--- src/components/result/welcome.tsx
+++ src/components/result/welcome.tsx
@@ -1,5 +1,6 @@
 import { COMMANDS, COMMANDS_HELP } from "@/commands";
-import { ShortDesc } from "../shortDesc";
+import { ShortDesc } from "@/components/shortDesc";
+import { Pfp } from "@/components/pfp";
 
 const desiredCommands = COMMANDS.filter(
   (command) =>
@@ -49,46 +50,3 @@ const Name = () => {
   ▀▀▀▀▀▀                                                 
     `}</pre>;
 };
-const Pfp = () => {
-  return <pre className="text-[10px] leading-[10px] col-start-2 row-start-1 -row-end-1">{`
-                                                                       
-                            .:+X$$$$X$X+.                              
-                       .:+$$$$$$$$X$&$$$XX;+                           
-                      :X$$XxXXX$$x$XX&X$$xxxX;                         
-                    ..$$XxXX$&&&&&$$$&&$$xX$X$$+                       
-            .   .  .$$$XXX$&&&&&&&&&&&&$X&&$$$$x   .                   
-           .  .    +$$$$XX&&&&&&$$Xxxxx$$&&&&&&&X.                     
-           ;:   ;;+$$$$$X$$&x;:.         .x&$$$$&x  ;                  
-         ;  ;$$X&&&$$$&XX&$X;.             X$$X$$&X.                   
-          .:   .+$$$&X$X$$$x;.             ;&&$$$$Xx:;:;+:             
-           x. :X$$$$$$X$$&$+:              .$&&&&&$$$$X; :             
-          ;:XX$$X$&&$X$$$&$$Xxx;:..;X&&&$$$++&&$&&&&$$x.               
-          $+$$&&&&&$$$$$&&&XX$$$X: ;x$$&$Xx. $&&&$$$$$$:               
-         $$+;$&$&&&&$&&&&&&$x+;;:.  .;+;;:   ;&&&&&&&$$$ X             
-       :xx xx$$&&&&&&&&$$&x.    ..           :$$$&&&&&$$ XX            
-        :$+;X$&$$$&&$$&$&X;     .            ;x:X&&&$+&;;: x:          
-         ;xX$$$$&&$$$XX$&X;.... +$$xX:  ;;.;.+::&&$&&$&+;.  x          
-     : : ;;$&&&$$$XX$&&&&&x;.:::X$&$&&$X$$;xxX&&&&&&&$x:.:. +          
-     :x$$&$x+&&$&&&&&&&&&&$xx&&$$x++;.   xX$&$&&&$$$&&&x.x.:           
-             &&&&&&&&$&&&&$&$X   .:;+;: .X$&&&&&&&$$$$&$x+XX.          
-         .+X&&&&&$$&$$&&&&&&&&X; :$&$: .X$&&&$&$&$&&&&&$+ .$x          
-           &$:$&$&&&&&&&$&&&&&&$xxxXXxXX&&&+.x&&&&&&&&&&$. ; ;         
-          :$xx&XX&&&&&&&X&&&+X&&&&&&&&&&&$.  +&&&$&&&&$X+              
-            XxXxX$&&&&&&&$&&+:;x$&&&&&$+     +&&$XX$&&XX;              
-               ;+x&&&$$$$$&&+:::;++Xx;:.     ;&$XXxX&&Xx+              
-     .        ;xxxx$$$$XX$$&x:..::;+x+:     .;$&Xxx$$xxx++.       +    
-             :xxXXxxX$$XXXX&$;:...::::.    ..+X&$xxXxxxx++x            
-            :+x+xXXXxxXXxxXX&X;:..:;;+;:. ..:+x$X$$xxxx+xx$$X+         
-       :   +$$Xx++x$$XxxxxxxX&&&;..:;+;:.....&$$$xxxXxX$X++$XxxXX      
-        ;XxxXXxxXX$XX$$Xxx+xx$&&&&&$x::. .x&&&$X+xxXX&&&&$x+$$+Xxxxx   
-      xx+xxXXx++xXX$&&$&$$Xx+xX$&&&$$&&&&&&&&&xxXX$&&$$XX$&Xx$XxXXx+x+ 
-    x++xxxxXx+x&&$XX$&&&&&&$$x+X$&&$X$$$$&&$X+++$&$&$x X$$&&$X$XxXXXx++
-  +x+xxxxxxXX&&&&&+$;;$&$&&&&xx;x+$&$XXX$$Xx$$x&&&$$XX$$+&&$$&&$xxXXXxx
- xXxXXxxxxXXX$&&$$&xXXXX$$&&&&x&&$XxX&XXXxX&&&x&&$&$XxXXx$$$x$&&$xxxXXX
-x$XXX$$xxX&&X$&$$$&XXxxXXX&&&&x&&&&$XXxx$&&&$$x&&$&XXxX+X$$$Xx&&&$XxX$$
-$$$$$$$XX$&&X&$X$$&&+XxxXXX&$$x&&$$X$$$$$$$$$$x&&$$XXXXX$XX$Xx$&&$$&$$$
-$$$&$$$XX&&$X&XX$$$&&xXxxxX&&$x$&&$XXxxxxxX$&$x&$$XXX$+$XXXXXxx$&&$&$$$
-$$&$&&&$$&&X$$XX$$$&&$XXxxX$&$x$&&$$XxxxxXX$&$x&X$XXXx&&$$$XXXXX&&$&&&&
-&&&&&&&&$&&XXXXXX$$&&&x$xxXX&$x$$&&$XXxxXXX$$$X&$$$X$X&$$$$$XXxX&&&&$&&
-    `}</pre>;
-}
